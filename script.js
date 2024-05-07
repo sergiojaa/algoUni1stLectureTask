fb6740b6fcf07bed32dc8dec5b2c4fd1cb933454
sergiojaa <119433399+sergiojaa@users.noreply.github.com>
2024-05-07 20:49:48 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+//                TASKS
+//1)დაწერეთ პროგრამა რომელიც კონსოლში გამოიტანს "Hello World" -ს.
+console.log('hello world')
+//2) შექმენით ცვლადი და მიანიჭეთ მას რიცხვი. დაბეჭდეთ ცვლადის მნიშვნელობა.
+const num = 5
+console.log(num)
+//3) დაწერეთ პროგრამა ორი რიცხვის შესაკრებად და შედეგის გამოსატანად.
+const num1 = 5
+const num2 = 10
+console.log(num1 + num2) 
+// ან პირდაპირ
+console.log(5 + 10)
+//4) დაწერეთ პროგრამა რომელიც გამოაკლებს ორ რიცხვს ერთმანეთს და აჩვენებს შედეგს.
+const number1 = 10
+const number2 = 5
+console.log(number1 - number2) 
+//5) დაწერეთ პროგრამა ორი რიცხვის გასამრავლებლად და შედეგის გამოსატანად.
+const numb1 = 5
+const numb2 = 5
+console.log(numb1 * numb2)
+//6) დაწერეთ პროგრამა ორი რიცხვის გასაყოფად და შედეგის გამოსატანად.
+const numbe1 = 50
+const numbe2 = 10
+console.log(numbe1 / numbe2)
+//7) დაწერეთ პროგრამა ორი რიცხვის გაყოფის ნაშთის გამოსათვლელად.
+const nu1 = 10
+const nu2 = 3
+console.log(nu1 % nu2)
+//8) შექმენით ცვლადი და მიანიჭეთ მას სტრიქონი. ამობეჭდეთ სტრიქონის სიგრძე.
+const strikoni = 'qaliashvili'
+console.log(strikoni.length)
+//9) დაწერეთ პროგრამა ორი სტრიქონის შესაერთებლად და შედეგის ჩვენების მიზნით.
+const str1 = 'sergi'
+const str2 = 'qaliashvili'
+console.log(str1 + ' '+ str2)
+//10) დაწერეთ პროგრამა სტრიქონის დიდ ასოებზე გადასაყვანად.
+const dzaglisName = 'vivieni'
+console.log(dzaglisName.toUpperCase())
+//11) დაწერეთ პროგრამა სტრიქონის მცირე ასოზე გადასაყვანად.
+const dzaglisSaxeli = 'ViViEnI'
+console.log(dzaglisSaxeli.toLowerCase())
+
+
